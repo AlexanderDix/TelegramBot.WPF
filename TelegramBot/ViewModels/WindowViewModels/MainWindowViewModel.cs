34aaaaee6b96a86fb804ab5742d31c7c2202f98f
AlexanderDix <axdxrere@gmail.com>
2022-08-03 15:13:47 +0500

    Creating a View model

---
+++ TelegramBot/ViewModels/WindowViewModels/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using TelegramBot.ViewModels.Base;
+
+namespace TelegramBot.ViewModels.WindowViewModels;
+
+internal class MainWindowViewModel : ViewModel
+{
+    #region Title : string - Заголовок окна
+
+    ///<summary>Заголовок окна</summary>
+    private string? _title;
+
+    ///<summary>Заголовок окна</summary>
+    public string? Title
+    {
+        get => _title;
+        set => Set(ref _title, value);
+    }
+
+    #endregion
+}
