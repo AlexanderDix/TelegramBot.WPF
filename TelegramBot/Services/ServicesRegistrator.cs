0336eb7f1248bbeae1ab742db692f6ae8bfa4ff4
AlexanderDix <axdxrere@gmail.com>
2022-08-18 10:52:21 +0500

    Промежуточные стили для элементов

--- TelegramBot/Services/ServicesRegistrator.cs
+++ TelegramBot/Services/ServicesRegistrator.cs
@@ -7,5 +7,6 @@ internal static class ServicesRegistrator
 {
     public static IServiceCollection AddServices(this IServiceCollection services) => services
         .AddTransient<IUserDialog, UserDialogService>()
-        .AddSingleton<BotManager>();
+        .AddSingleton<BotManager>()
+        .AddSingleton<MessageManager>();
 }
