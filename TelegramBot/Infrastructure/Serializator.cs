f7f9cc7a591fdc8015d83481392a9aa78d5e3b9b
AlexanderDix <axdxrere@gmail.com>
2022-08-18 10:51:16 +0500

    Класс с методами сериализации данных в формат JSON

---
+++ TelegramBot/Infrastructure/Serializator.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace TelegramBot.Infrastructure;
+
+internal static class Serializator
+{
+    public static void Serialize<T>(T source, string path = "senders.json")
+    {
+        var json = JsonConvert.SerializeObject(source);
+
+        File.WriteAllText(path, json);
+    }
+
+    public static T Deserialize<T>(string path = "senders.json")
+    {
+        var json = File.ReadAllText(path);
+
+        return JsonConvert.DeserializeObject<T>(json);
+    }
+}
