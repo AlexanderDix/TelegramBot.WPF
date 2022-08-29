ac7c6fd61c5af77cb519b5de5f438c6aa307e2ee
AlexanderDix <axdxrere@gmail.com>
2022-08-29 14:47:39 +0500

    Добавление класса Configuration.cs для хранения токенов

---
+++ ConsoleTelegramBot/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace ConsoleTelegramBot;
+
+internal static class Configuration
+{
+    public const string ApiKey = "777d2e2755efc01eb5e084742f87f20b";
+
+    public const string Token = "2016907380:AAHuxyf6A7MSLOm4hCnhxycmlDDvuzi982s";
+}
---
+++ TelegramBot/Infrastructure/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace TelegramBot.Infrastructure;
+
+internal static class Configuration
+{
+    public const string ApiKey = "ApiKey OpenWeatherMap";
+
+    public const string Token = "Telegram Bot Token";
+}
